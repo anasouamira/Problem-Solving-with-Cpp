8b00c98a80dc98dd15773c009754f6021a9f5ad5
Ouamira <anasouamira@gmail.com>
2024-11-11 18:05:45 +0100

    Created By Anas Oumaira

---
+++ Problem solving with c++/Problem solving Level 2/Problem15.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+short RedNumberPsitive(string message)
+{
+    short Number;
+    do
+    {
+        cout << message;
+        cin >> Number;
+
+    } while (Number < 0);
+
+    return Number;
+}
+void PrintInvertedPattern(short Number)
+{
+
+    int final_letter = 65 + (Number - 1);
+    int counter = 1;
+
+    for (int i = 65; i <= final_letter; i++)
+    {
+        for (int j = 1; j <= counter; j++)
+            cout << char(i);
+        counter++;
+        if (counter > Number)
+            break;
+        cout << endl;
+    }
+}
+
+int main()
+{
+    PrintInvertedPattern(RedNumberPsitive("Enter Number to print Inverted Pattern : "));
+}
