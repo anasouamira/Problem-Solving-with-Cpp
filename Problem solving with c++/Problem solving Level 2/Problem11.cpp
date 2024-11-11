1376f226e255a734e34fbf08141517221c12be46
Ouamira <anasouamira@gmail.com>
2024-11-11 15:01:03 +0100

    Created By Anas Oamira

---
+++ Problem solving with c++/Problem solving Level 2/Problem11.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int RedPositiveNumbers(string message)
+{
+
+    int Number;
+    do
+    {
+        cout << message;
+        cin >> Number;
+
+    } while (Number < 0);
+
+    return Number;
+}
+int ReversNumber(int Number)
+{
+
+    short Remainder = 0;
+    int Revers = 0;
+
+    while (Number > 0)
+    {
+        Remainder = Number % 10;
+        Number = Number / 10;
+        Revers = Revers * 10 + Remainder;
+    }
+    return Revers;
+}
+string CheckPalindromeNumber(int Number, int ReversNumber)
+{
+    if (Number == ReversNumber)
+        return "Yes, it is a Palindrome number.";
+    return "No, it is a NOT Palindrome number.";
+}
+
+int main()
+{
+    int Number = RedPositiveNumbers("Enter Number to check it if a Palindrome Number : ");
+    int Revers_Number = ReversNumber(Number);
+
+    cout << CheckPalindromeNumber(Number, Revers_Number) << endl;
+}
