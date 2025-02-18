9ae99173bbd917baba99c40def4e6962833283fa
Ouamira <anasouamira@gmail.com>
2025-02-18 22:50:38 +0100

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 2/Problem48.cpp
+++ Problem solving with c++/Problem solving Level 2/Problem48.cpp
@@ -27,25 +27,39 @@ C++ Round Result: -11
 */
 #include <iostream>
 using namespace std;
+
+// Function to find the floor of a floating-point number
+// Floor of a number is the greatest integer less than or equal to the number
 int MyFloor(float Number)
 {
+    // If the number is positive, the floor is simply the integer part
     if (Number > 0)
         return int(Number);
     else
+        // If the number is negative and not an exact integer, subtract 1
         return int(Number) - 1;
 }
 
+// Function to read a floating-point number from the user
 float ReadNumber()
 {
     float Number;
-    cout << "Please enter a number?";
+    cout << "Please enter a number? ";
     cin >> Number;
     return Number;
 }
+
 int main()
 {
+    // Read the number from the user
     float Number = ReadNumber();
+
+    // Display the result of custom floor calculation using MyFloor()
     cout << "My MyFloor Result : " << MyFloor(Number) << endl;
+
+    // Display the result of standard C++ floor calculation using floor()
     cout << "C++ floor Result  : " << floor(Number) << endl;
+
     return 0;
 }
+
