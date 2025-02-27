374edee6ae9869f85e6bf0ef420b6a84873fd385
Ouamira <anasouamira@gmail.com>
2025-02-27 23:32:33 +0000

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 2/Problem49.cpp
+++ Problem solving with c++/Problem solving Level 2/Problem49.cpp
@@ -18,3 +18,41 @@ My Round Result : -10
 C++ Round Result: -10
 =============================================================
 */
+
+#include <iostream>
+#include <cmath> // Include cmath for ceil()
+using namespace std;
+
+// Function to get the fractional part of a floating-point number
+float GetFractionPart(float Number)
+{
+    return Number - int(Number);
+}
+
+// Custom implementation of ceil function
+int MyCeil(float Number)
+{
+    int integerPart = int(Number); // Get integer part
+    if (Number > 0 && GetFractionPart(Number) > 0) 
+        return integerPart + 1;
+    return integerPart;
+}
+
+// Function to read a number from user input
+float ReadNumber()
+{
+    float Number;
+    cout << "Please enter a number: ";
+    cin >> Number;
+    return Number;
+}
+
+// Main function
+int main()
+{
+    float Number = ReadNumber();
+    cout << "My MyCeil Result: " << MyCeil(Number) << endl;
+    cout << "C++ ceil Result: " << ceil(Number) << endl;
+    return 0;
+}
+
