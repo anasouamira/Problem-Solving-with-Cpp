fdc03d86f59ef91a15928fd29f3fd6145a64cc5d
Ouamira <anasouamira@gmail.com>
2025-03-09 23:19:35 +0000

    Created By Anas Ouamira

--- Problem solving with Python/Problem26.cpp
+++ Problem solving with Python/Problem26.cpp
@@ -1,4 +1,4 @@
-/*
+"""
 Problem 26 |=================================================
 Write a program to fill array with max size 100 with random
 numbers from 1 to 100, then print sum of all number
@@ -11,8 +11,8 @@ Output:
 Array Elements: 10 6 22 40 12 75 23 32 25 75
 Sum of all numbers:320
 =============================================================
+"""
 
-*/
 #include <iostream>
 using namespace std;
 
