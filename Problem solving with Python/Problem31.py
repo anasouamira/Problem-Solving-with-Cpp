80007364a0b7ca6ae12ba4eb7a147dad98c0e2f9
Ouamira <anasouamira@gmail.com>
2025-03-10 23:33:16 +0000

    Created By Anas Ouamira

--- Problem solving with Python/Problem31.py
+++ Problem solving with Python/Problem31.py
@@ -1,4 +1,4 @@
-/*
+"""
 Problem 31 |=================================================
 Write a program to fill array with ordered numbers from 1 to N,
 then print it, after that shuffle this array and print it after shuffle.
@@ -14,80 +14,56 @@ Array elements before shuffle:
 Array elements after shuffle:
 2 4 6 3 10 1 7 8 5 9
 =============================================================
-*/
-#include <iostream>
-using namespace std;
+"""
+import random
 
-// Function to read a positive number from the user
-int ReadPositiveNumber(string Message)
-{
-    int Number = 0;
-    do
-    {
-        cout << Message << endl;
-        cin >> Number;
-    } while (Number <= 0);
-    return Number;
-}
+def read_positive_number(message: str) -> int:
+    """
+    Reads a positive integer from the user.
+    Keeps prompting until a valid positive integer is entered.
+    """
+    while True:
+        try:
+            number = int(input(message))
+            if number > 0:
+                return number
+            print("Please enter a positive number.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
 
-// Function to swap two integers by reference
-void Swap(int &A, int &B)
-{
-    int Temp;
-    Temp = A;
-    A = B;
-    B = Temp;
-}
+def fill_array_with_1_to_n(arr_length: int) -> list:
+    """
+    Fills a list with values from 1 to N.
+    """
+    return list(range(1, arr_length + 1))
 
-// Function to fill an array with values from 1 to N
-void FillArrayWith1toN(int arr[100], int arrLength)
-{
-    for (int i = 0; i < arrLength; i++)
-        arr[i] = i + 1;
-}
+def shuffle_array(arr: list):
+    """
+    Shuffles the array randomly in place.
+    """
+    random.shuffle(arr)
 
-// Function to generate a random number within a given range
-int RandomNumber(int From, int To)
-{
-    int randNum = rand() % (To - From + 1) + From;
-    return randNum;
-}
+def print_array(arr: list):
+    """
+    Prints the elements of an array.
+    """
+    print(" ".join(map(str, arr)))
 
-// Function to shuffle an array randomly
-void ShuffleArray(int arr[100], int arrLength)
-{
-    for (int i = 0; i < arrLength; i++)
-    {
-        Swap(arr[RandomNumber(1, arrLength) - 1],
-            arr[RandomNumber(1, arrLength) - 1]);
-    }
-}
+if __name__ == "__main__":
+    # Seed the random number generator
+    random.seed()
 
-// Function to print an array
-void PrintArray(int arr[100], int arrLength)
-{
-    for (int i = 0; i < arrLength; i++)
-        cout << arr[i] << " ";
-    cout << "\n";
-}
+    # Read the number of elements
+    arr_length = read_positive_number("\nEnter number of elements:\n")
 
-int main()
-{
-    // Seeds the random number generator in C++, called only once
-    srand((unsigned)time(NULL));
-    
-    int arr[100];
-    int arrLength = ReadPositiveNumber("\nEnter number of elements : \n");
-    
-    FillArrayWith1toN(arr, arrLength);
-    
-    cout << "\nArray elements before shuffle:\n";
-    PrintArray(arr, arrLength);
-    
-    ShuffleArray(arr, arrLength);
-    
-    cout << "\nArray elements after shuffle:\n";
-    PrintArray(arr, arrLength);
-    
-    return 0;
-}
+    # Generate an ordered array from 1 to N
+    arr = fill_array_with_1_to_n(arr_length)
+
+    print("\nArray elements before shuffle:")
+    print_array(arr)
+
+    # Shuffle the array
+    shuffle_array(arr)
+
+    print("\nArray elements after shuffle:")
+    print_array(arr)
