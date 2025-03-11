3d0e7b297be071f4d8e0c5454c72e2a45452b350
Ouamira <anasouamira@gmail.com>
2025-03-11 02:45:31 +0000

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 2/Problem36.cpp
+++ Problem solving with c++/Problem solving Level 2/Problem36.cpp
@@ -7,13 +7,13 @@ Output:
 
 Please enter a number? 10
 
-Do you want to add more numbers? [0}No,[1 yes? 1
+Do you want to add more numbers? [0] No,[1] yes? 1
 Please enter a number? 20
 
-Do you want to add more numbers? [0}No,[1 yes? 1
+Do you want to add more numbers? [0] No,[1] yes? 1
 Please enter a number? 30
 
-Do you want to add more numbers? [0JNo [1}yes? 0
+Do you want to add more numbers? [0] No [1] yes? 0
 
 Array Length: 3
 Array elements: 10 20 30
