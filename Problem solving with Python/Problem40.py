43a2939f5cb55a63194ce716f3c8f15043c011d2
Ouamira <anasouamira@gmail.com>
2025-03-16 22:33:15 +0000

    Created By Anas Ouamira

--- Problem solving with Python/Problem40.py
+++ Problem solving with Python/Problem40.py
@@ -1,4 +1,4 @@
-/*
+"""
 Problem 40 |=================================================
 Write a program to fill array with numbers, then print distinct
 numbers to another array.
@@ -12,7 +12,7 @@ Array 1 elements:
 Array 2 distinct elements:
 10 50 70 90
 =============================================================
-*/
+"""
 #include <iostream>
 using namespace std;
 
