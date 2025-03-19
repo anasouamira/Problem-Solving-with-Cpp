f0d6dc771535fe331f44523650d3e29d14fb00bb
Ouamira <anasouamira@gmail.com>
2025-03-19 21:28:50 +0000

    Created By Anas Ouamira

--- Problem solving with Python/Problem42.py
+++ Problem solving with Python/Problem42.py
@@ -1,4 +1,4 @@
-/*
+"""
 Problem 42 |=================================================
 Write a program to fill array with max size 100 with random
 numbers from 1 to 100, then print the count of Odd numbers.
@@ -10,7 +10,7 @@ Output:
 Array Elements: 60 78 15 49 56 6 4 3 21 23 
 Odd Numbers count is: 5
 =============================================================
-*/
+"""
 #include <iostream>
 using namespace std;
 
