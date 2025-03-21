6f6da4a037578f22c9696c919c0efbd46c3611c4
Ouamira <anasouamira@gmail.com>
2025-03-21 22:09:42 +0000

    Created By Anas Ouamira

--- Problem solving with Python/Problem46.py
+++ Problem solving with Python/Problem46.py
@@ -1,4 +1,5 @@
-/*
+
+"""
 Problem 46 |====================================================
 Write a program to print abs of numbers , don't use built in abs 
 function 
@@ -11,7 +12,7 @@ Output:
 My abs Result : 10
 C++ abs Result: 10
 ================================================================
-*/
+"""
 #include <iostream>
 using namespace std;
 
