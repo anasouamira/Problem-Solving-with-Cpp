28664bddf8d906184bc68206b1c7ca934cc407a4
Ouamira <anasouamira@gmail.com>
2025-03-22 21:31:29 +0000

    Created By Anas Ouamira

--- Problem solving with Python/Problem49.cpp
+++ Problem solving with Python/Problem49.cpp
@@ -1,4 +1,4 @@
-/*
+"""
 Problem 49 |=================================================
 Write a program to print Ceil of numbers , don't use built in Ceil
 function
@@ -17,7 +17,7 @@ Output:
 My Round Result : -10
 C++ Round Result: -10
 =============================================================
-*/
+"""
 
 #include <iostream>
 #include <cmath> // Include cmath for ceil()
