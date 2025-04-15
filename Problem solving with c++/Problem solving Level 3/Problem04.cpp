119c127f5987ef4cd6815ebd592c5ca87416a889
Ouamira <anasouamira@gmail.com>
2025-04-15 07:15:38 +0100

    Created By Anas Ouamira

---
+++ Problem solving with c++/Problem solving Level 3/Problem04.cpp
@@ -0,0 +1,23 @@
+/*
+Problem 01 |=================================================
+
+Write a program fo fill a 3x3 matrix with random numbers,
+then print each row sum :
+
+Output :
+
+The following is a 3x3 random matrix:
+
+16 43 6
+13 51 3
+24 19 100
+
+The the following are the sum of each row in the matrix:
+Row 1 Sum = 102
+
+Row 2 Sum = 196
+
+Row 3 Sum = 130
+
+=============================================================
+*/
