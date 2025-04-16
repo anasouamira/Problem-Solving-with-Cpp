059cbc600de6d28141b85fa3ee487eb73844d0ea
Ouamira <anasouamira@gmail.com>
2025-04-16 19:36:14 +0100

    Created By Anas Ouamira

---
+++ Problem solving with c++/Problem solving Level 3/Problem14.cpp
@@ -0,0 +1,77 @@
+/*
+Problem 01 |=================================================
+
+Write a program to check if the matrix is identity or not.
+
+Output :
+
+Matrix1: 
+1  0  0
+0  1  0
+0  0  1
+
+YES: Matrix is identity.
+=============================================================
+*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Function to print a matrix
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            cout << setw(3) << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// Function to check if the matrix is a scalar matrix
+bool IsScalarMatrix(int Matrix1[3][3], short Rows, short Cols)
+{
+    // Get the value of the first diagonal element to compare with others
+    int diagonalValue = Matrix1[0][0];
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            if (i == j)
+            {
+                // Diagonal elements must all be equal to the first one
+                if (Matrix1[i][j] != diagonalValue)
+                    return false;
+            }
+            else
+            {
+                // Non-diagonal elements must be 0
+                if (Matrix1[i][j] != 0)
+                    return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int Matrix1[3][3] = {
+        {9, 0, 0},
+        {0, 9, 0},
+        {0, 0, 9}
+    };
+
+    cout << "\nMatrix1:\n";
+    PrintMatrix(Matrix1, 3, 3);
+
+    if (IsScalarMatrix(Matrix1, 3, 3))
+        cout << "\nYES: Matrix is scalar.";
+    else
+        cout << "\nNO: Matrix is NOT scalar.";
+
+    system("pause>0");
+}
