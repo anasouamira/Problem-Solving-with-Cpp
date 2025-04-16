069e0eb2b93d6666724388d1ed99689ea434525b
Ouamira <anasouamira@gmail.com>
2025-04-16 23:10:28 +0100

    Created By Anas Ouamira

---
+++ Problem solving with c++/Problem solving Level 3/Problem16.cpp
@@ -0,0 +1,15 @@
+/*
+Problem 01 |=================================================
+
+Write a program to check if the matrix is Sparse or not.
+
+Output :
+
+Matrix1: 
+0  0   1
+0  0   12
+0  0   5
+
+Yes: It is Sparse
+=============================================================
+*/
