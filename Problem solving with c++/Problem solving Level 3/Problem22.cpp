4c24f6d757f441c2eaf6669e13aa9f0a88cf1981
Ouamira <anasouamira@gmail.com>
2025-04-18 08:09:20 +0100

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 3/Problem22.cpp
+++ Problem solving with c++/Problem solving Level 3/Problem22.cpp
@@ -7,5 +7,26 @@ Output :
 
 1  1  2  3  5  8  13  21  34  55
 
+With Recursion Function
 =============================================================
 */
+#include <iostream>
+using namespace std;
+void PrintFibonacciUsingRecurssion(short Number, int Prev1, int
+Prev2)
+{
+int FebNumber = 0;
+if (Number > 0)
+{
+FebNumber = Prev2 + Prev1;
+Prev2 = Prev1;
+Prev1 = FebNumber;
+cout << FebNumber << " ";
+PrintFibonacciUsingRecurssion(Number - 1, Prev1, Prev2);
+}
+}
+int main()
+{
+PrintFibonacciUsingRecurssion(10, 0, 1);
+system("pause>0");
+}
