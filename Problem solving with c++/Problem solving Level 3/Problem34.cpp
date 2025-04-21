a864634339c3128252b27194aa861b0c97268781
Ouamira <anasouamira@gmail.com>
2025-04-21 09:10:34 +0100

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 3/Problem34.cpp
+++ Problem solving with c++/Problem solving Level 3/Problem34.cpp
@@ -1,5 +1,5 @@
 /*
-Problem 27 |=================================================
+Problem 34 |=================================================
 
 Write a program to read a string then print all vowels in that
 string (Vowels are: a,e,i,o,u)
