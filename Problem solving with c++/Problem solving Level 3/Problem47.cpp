cd779112c05521f301b2bc08282c6445913d6bdc
Ouamira <anasouamira@gmail.com>
2025-04-25 23:44:51 +0100

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 3/Problem47.cpp
+++ Problem solving with c++/Problem solving Level 3/Problem47.cpp
@@ -21,7 +21,11 @@ Client Added Successfully, do you want to add more clients :
 #include <string>
 #include <vector>
 using namespace std;
+
+// Define a constant for the file name to store client data
 const string ClientsFileName = "Clients.txt";
+
+// Define the structure to represent a client's information
 struct sClient
 {
     string AccountNumber;
@@ -30,63 +34,85 @@ struct sClient
     string Phone;
     double AccountBalance;
 };
+
+// Function to read new client details from the user
 sClient ReadNewClient()
 {
     sClient Client;
+
     cout << "Enter Account Number? ";
-    // Usage of std::ws will extract allthe whitespace character
-    getline(cin >> ws, Client.AccountNumber);
+    getline(cin >> ws, Client.AccountNumber); // 'ws' clears leading whitespaces
+
     cout << "Enter PinCode? ";
     getline(cin, Client.PinCode);
+
     cout << "Enter Name? ";
     getline(cin, Client.Name);
+
     cout << "Enter Phone? ";
     getline(cin, Client.Phone);
+
     cout << "Enter AccountBalance? ";
     cin >> Client.AccountBalance;
+
     return Client;
 }
-string ConvertRecordToLine(sClient Client, string Seperator ="#//#")
+
+// Function to convert a client structure into a formatted string line
+string ConvertRecordToLine(sClient Client, string Seperator = "#//#")
 {
     string stClientRecord = "";
+
+    // Concatenate fields with a custom separator
     stClientRecord += Client.AccountNumber + Seperator;
     stClientRecord += Client.PinCode + Seperator;
     stClientRecord += Client.Name + Seperator;
     stClientRecord += Client.Phone + Seperator;
-    stClientRecord += to_string(Client.AccountBalance);
+    stClientRecord += to_string(Client.AccountBalance); // Convert balance to string
+
     return stClientRecord;
 }
+
+// Function to append a line of client data to a file
 void AddDataLineToFile(string FileName, string stDataLine)
 {
     fstream MyFile;
-    MyFile.open(FileName, ios::out | ios::app);
+    MyFile.open(FileName, ios::out | ios::app); // Open file in append mode
+
     if (MyFile.is_open())
     {
-        MyFile << stDataLine << endl;
-        MyFile.close();
+        MyFile << stDataLine << endl; // Write line to file
+        MyFile.close(); // Close file
     }
 }
+
+// Function to read a client from the user and add to the file
 void AddNewClient()
 {
-    sClient Client;
-    Client = ReadNewClient();
-    AddDataLineToFile(ClientsFileName,ConvertRecordToLine(Client));
+    sClient Client = ReadNewClient(); // Read client details
+    AddDataLineToFile(ClientsFileName, ConvertRecordToLine(Client)); // Save to file
 }
+
+// Function to repeatedly add clients until the user stops
 void AddClients()
 {
     char AddMore = 'Y';
+
     do
     {
-        system("cls");
+        system("cls"); // Clear screen
         cout << "Adding New Client:\n\n";
-        AddNewClient();
-        cout << "\nClient Added Successfully, do you want to addmore clients? Y/N? ";
+        AddNewClient(); // Add one client
+
+        cout << "\nClient Added Successfully, do you want to add more clients? Y/N? ";
         cin >> AddMore;
-    } while (toupper(AddMore) == 'Y');
+
+    } while (toupper(AddMore) == 'Y'); // Repeat if user says yes
 }
+
 int main()
 {
-    AddClients();
-    system("pause>0");
+    AddClients(); // Start the client-adding process
+    system("pause>0"); // Pause the program (Windows only)
     return 0;
 }
---
+++ Problem solving with c++/Problem solving Level 3/Problem48.cpp
@@ -0,0 +1,155 @@
+/*
+Problem 27 |=================================================
+
+Write a program to convert line data fo record and print it:
+
+Adding New Client:
+
+Client Data:
+Enter Account Number :  A150
+Enter PinCode :  1234
+Enter Name :  Mohanmed Abu-Hadhoud
+Enter Phone :  079939999
+Enter AccountBalance :  5660
+
+Client Added Successfully, do you want to add more clients : 
+
+===============================================================
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+const string ClientsFileName = "Clients.txt"; // File to store clients
+
+// Struct to represent a client's data
+struct sClient
+{
+    string AccountNumber;
+    string PinCode;
+    string Name;
+    string Phone;
+    double AccountBalance;
+};
+
+// Function to split a string based on a delimiter
+vector<string> SplitString(string S1, string Delim)
+{
+    vector<string> vString;
+    short pos = 0;
+    string sWord;
+
+    // Loop to extract words until no more delimiters are found
+    while ((pos = S1.find(Delim)) != std::string::npos)
+    {
+        sWord = S1.substr(0, pos); // Extract word before delimiter
+        if (sWord != "")
+        {
+            vString.push_back(sWord); // Add word to vector
+        }
+        S1.erase(0, pos + Delim.length()); // Remove word and delimiter
+    }
+
+    // Add the last word (if any)
+    if (S1 != "")
+    {
+        vString.push_back(S1);
+    }
+
+    return vString;
+}
+
+// Function to convert a line from the file into a client record
+sClient ConvertLinetoRecord(string Line, string Seperator = "#//#")
+{
+    sClient Client;
+    vector<string> vClientData = SplitString(Line, Seperator);
+
+    // Assign data to client struct fields
+    Client.AccountNumber = vClientData[0];
+    Client.PinCode = vClientData[1];
+    Client.Name = vClientData[2];
+    Client.Phone = vClientData[3];
+    Client.AccountBalance = stod(vClientData[4]); // Convert balance to double
+
+    return Client;
+}
+
+// Function to read all clients from file and return a vector of clients
+vector<sClient> LoadCleintsDataFromFile(string FileName)
+{
+    vector<sClient> vClients;
+    fstream MyFile;
+    MyFile.open(FileName, ios::in); // Open file in read mode
+
+    if (MyFile.is_open())
+    {
+        string Line;
+        sClient Client;
+
+        // Read each line and convert to client object
+        while (getline(MyFile, Line))
+        {
+            Client = ConvertLinetoRecord(Line);
+            vClients.push_back(Client); // Add to vector
+        }
+
+        MyFile.close(); // Close file
+    }
+
+    return vClients;
+}
+
+// Function to print a single client record in a formatted way
+void PrintClientRecord(sClient Client)
+{
+    cout << "| " << setw(15) << left << Client.AccountNumber;
+    cout << "| " << setw(10) << left << Client.PinCode;
+    cout << "| " << setw(40) << left << Client.Name;
+    cout << "| " << setw(12) << left << Client.Phone;
+    cout << "| " << setw(12) << left << Client.AccountBalance;
+}
+
+// Function to print all clients with headers and formatting
+void PrintAllClientsData(vector<sClient> vClients)
+{
+    cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+
+    // Print table headers
+    cout << "| " << left << setw(15) << "Accout Number";
+    cout << "| " << left << setw(10) << "Pin Code";
+    cout << "| " << left << setw(40) << "Client Name";
+    cout << "| " << left << setw(12) << "Phone";
+    cout << "| " << left << setw(12) << "Balance";
+
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+
+    // Print all client records
+    for (sClient Client : vClients)
+    {
+        PrintClientRecord(Client);
+        cout << endl;
+    }
+
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n" << endl;
+}
+
+int main()
+{
+    // Load clients from file
+    vector<sClient> vClients = LoadCleintsDataFromFile(ClientsFileName);
+
+    // Print all loaded client data
+    PrintAllClientsData(vClients);
+
+    system("pause>0"); // Wait for user before exiting (Windows only)
+    return 0;
+}
+
