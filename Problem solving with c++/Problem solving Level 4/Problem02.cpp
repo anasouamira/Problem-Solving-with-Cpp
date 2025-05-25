680f30f1f52f8ce5c545099870b5e0fb31331f11
Ouamira <anasouamira@gmail.com>
2025-05-25 19:58:14 +0100

    Created By Anas Ouamira

---
+++ Problem solving with c++/Problem solving Level 4/Problem02.cpp
@@ -0,0 +1,21 @@
+/*
+Problem 01 |=============================================================================================
+
+Write a program fo read a number and
+print the text of that number : 
+
+Input : 
+Enter a Number? 5843 
+
+Enter a Number? 8476362741 
+
+
+Output :
+
+Five Thousands Eight Hundreds Forty Three 
+
+Eight billion four hundred seventy six million three hundred sixty two thousand seven hundred forty one.
+==========================================================================================================
+*/
+
+
