95fcd6d84b81ade0c189a54a6229662c72547319
Ouamira <anasouamira@gmail.com>
2025-05-27 21:39:35 +0100

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 4/Problem11.cpp
+++ Problem solving with c++/Problem solving Level 4/Problem11.cpp
@@ -2,7 +2,9 @@
 Problem 01 |=============================================================================================
 
 Write a program to print total days
-from the beginning of year.
+from the beginning of year, then
+Take the total days and convert them
+back to date.
 
 ==========================================================================================================
 */
