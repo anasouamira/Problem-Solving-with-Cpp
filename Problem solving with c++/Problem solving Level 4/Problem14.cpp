792b16a4719098c97d9664968e499253b9d83982
Ouamira <anasouamira@gmail.com>
2025-06-17 12:42:52 +0100

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 4/Problem14.cpp
+++ Problem solving with c++/Problem solving Level 4/Problem14.cpp
@@ -1,9 +1,8 @@
 /*
 Problem 01 |=============================================================================================
 
-Write a program to read date and read
-how many days to add to it, print the
-results on screen
+Write a program to read datel, date2 and
+check if Datel Equals to Date2
 
 ==========================================================================================================
 */
