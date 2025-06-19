8c0dd5cf051f14ec9e33b750b1deeafd1cb9705c
Ouamira <anasouamira@gmail.com>
2025-06-19 20:27:59 +0100

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 4/Problem20_to_32.cpp
+++ Problem solving with c++/Problem solving Level 4/Problem20_to_32.cpp
@@ -1,9 +1,34 @@
 /*
 Problem 01 |=============================================================================================
 
-Write a program to read date and read
-how many days to add to it, print the
-results on screen
+Write a program to read a date and make a functions
+to increase date as follows:
+
++ IncreaseDateByXDays
+
++ IncreaseDateByOneWeek
+
++ IncreaseDateByXWeeks
+
++ IncreaseDateByOneMonth
+
++ IncreaseDateByXMonths
+
++ IncreaseDateByOneYear
+
++ IncreaseDateByXYears
+
++ IncreaseDateByXYearsFaster
+
++ IncreaseDateByOneDecade
+
++ IncreaseDateByXDecades
+
++ IncreaseDateByXDecadesFaster
+
++ IncreaseDateByOneCentury
+
++ IncreaseDateByOneMillennium
 
 ==========================================================================================================
 */
