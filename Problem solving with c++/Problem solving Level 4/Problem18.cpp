df2a7d5117dacd0ce64d25ed730ef8c63371d6e6
Ouamira <anasouamira@gmail.com>
2025-06-19 19:30:03 +0100

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 4/Problem18.cpp
+++ Problem solving with c++/Problem solving Level 4/Problem18.cpp
@@ -1,11 +1,12 @@
 /*
 Problem 01 |=============================================================================================
 
-Write a program to read date and read
-how many days to add to it, print the
-results on screen
+Werite a program calculate you age in days.
 
 ==========================================================================================================
 */
 
-
+#pragmawarning(disable : 4996) #include<iostream>usingnamespace std; structstDate{ short Year; short Month; short Day; }; bool isLeapYear(shortYear) { return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0); } bool IsDate1BeforeDate2(stDateDate1, stDateDate2) { return  (Date1.Year < Date2.Year) ? true : ((Date1.Year == Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month == Date2.Month ? Date1.Day < Date2.Day : false)) : false); } short NumberOfDaysInAMonth(shortMonth, shortYear) { if (Month < 1 || Month>12) return  0; int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 }; return (Month == 2) ? (isLeapYear(Year) ? 29 : 28) : days[Month - 1]; } bool IsLastDayInMonth(stDateDate) { return (Date.Day == NumberOfDaysInAMonth(Date.Month, Date.Year)); } bool IsLastMonthInYear(shortMonth) { return (Month == 12); } 
+stDate IncreaseDateByOneDay(stDateDate) { if (IsLastDayInMonth(Date))     { if (IsLastMonthInYear(Date.Month))         { Date.Month = 1; Date.Day = 1; Date.Year++;         } else        { Date.Day = 1; Date.Month++;         }     } else    { Date.Day++;     } returnDate; } int GetDifferenceInDays(stDateDate1, stDateDate2, boolIncludeEndDay = false) { int Days = 0; while (IsDate1BeforeDate2(Date1, Date2))     {         Days++; Date1 = IncreaseDateByOneDay(Date1);     } returnIncludeEndDay ? ++Days : Days; } short ReadDay() { short Day;     cout << "\nPlease enter a Day? ";     cin >> Day; return Day; } 
+short ReadMonth() { short Month;     cout << "Please enter a Month? ";     cin >> Month; return Month; } short ReadYear() { short Year;     cout << "Please enter a Year? ";     cin >> Year; return Year; } stDate ReadFullDate() { stDate Date;     Date.Day = ReadDay();     Date.Month = ReadMonth();     Date.Year = ReadYear(); return Date; } stDate GetSystemDate() { stDate Date; time_t t = time(0); tm* now = localtime(&t);     Date.Year = now->tm_year + 1900;     Date.Month = now->tm_mon + 1;     Date.Day = now->tm_mday; return Date; } 
+int main() {     cout << "\nPlease Enter Your Date of Birth:\n"; stDate Date1 = ReadFullDate(); stDate Date2 = GetSystemDate();     cout << "\nYour Age is : "<< GetDifferenceInDays(Date1, Date2, true) << " Day(s).";     system("pause>0"); return 0; }
