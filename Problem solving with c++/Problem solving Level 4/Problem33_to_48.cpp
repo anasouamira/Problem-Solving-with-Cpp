0ca3d1db0bd0871994ef8fac00dfe3aa9418608c
Ouamira <anasouamira@gmail.com>
2025-06-25 19:40:12 +0100

    Created By Anas Ouamira

--- Problem solving with c++/Problem solving Level 4/Problem33_to_48.cpp
+++ Problem solving with c++/Problem solving Level 4/Problem33_to_48.cpp
@@ -1,9 +1,36 @@
 /*
 Problem 21 |=============================================================================================
 
-Write a program to read date and read
-how many days to add to it, print the
-results on screen
+Write a program to read a date and make a functions to
+Decrease date as follows:
+
++ DecreaseDateByOneDay
+
++ DecreaseDateByXDays
+
++ DecreaseDateByOneWeek
+
++ DecreaseDateByXWeeks
+
++ DecreaseDateByOneMonth
+
++ DecreaseDateByXMonths
+
++ DecreaseDateByOneYear
+
++ DecreaseDateByXYears
+
++ DecreaseDateByXYearsFaster 
+
++ DecreaseDateByOneDecade 
+
++ DecreaseDateByXDecades
+
++ DecreaseDateByXDecadesFaster
+
++ DecreaseDateByOneCentury
+
++ DecreaseDateByOneMillennium
 
 ==========================================================================================================
 */
